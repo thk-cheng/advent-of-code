7168fec4c5a959863bd14872ad06542104c3471b
thk-cheng <kenneth.cheng.tsun.him@gmail.com>
2022-08-04 18:47:13 +0100

    Updated 2015 day 05 part 02
    Signed-off-by: thk-cheng <kenneth.cheng.tsun.him@gmail.com>

--- 2015/day_05/day_05_solution_part_02.py
+++ 2015/day_05/day_05_solution_part_02.py
@@ -9,17 +9,26 @@ def count_nice(strings: list[str]) -> int:
     '''Count the number of strings that are nice'''
     nice_cnt = 0
     for s in strings:
+        # Initialize counter for the requirements
         pair_cnt = 0
         repeat_cnt = 0
+
+        # Loop over each letter in each string
         for idx, l in enumerate(s):
+            # Check pair condition
             if idx < len(s) - 3:
                 target = s[idx:idx + 2]
+                # Loop over all the pairwise letters after the target, see if any of them match
                 for j in range(idx + 2, len(s) - 1):
                     if target == s[j:j + 2]:
                         pair_cnt += 1
+
+            # Check repeat condition
             if idx < len(s) - 2:
                 if l == s[idx + 2]:
                     repeat_cnt += 1
+
+        # Increment the counter for nice string if both conditions are met
         if (pair_cnt >= 1) and (repeat_cnt >=1):
             nice_cnt += 1
     return nice_cnt
